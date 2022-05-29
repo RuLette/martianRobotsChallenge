3e705cdf06af40f56cf31aff2b73271553b858e2
Ru L <rul@Rus-MacBook-Air.local>
2022-05-29 19:02:39 +0100

    sanitized input data and adding testing for functions formatting inputs

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
 const compass = ["N", "E", "S", "W"];
 
 let coordinates = { x: "", y: "", pos: "" };
-let grid = [];
+let gridMaxCoordinates = [];
 
 function moveRobot(command) {
   //Takes in command "L", "F", etc
   //Needs to filter each command
-  //North + L = East, anything else is compass value of index - 1 or + 1
+  let index;
+  switch (command) {
+    case "L":
+    //North + L = East, anything else is compass value of index - 1 or + 1
+    case "R":
+    //compass value of index - 1 or + 1
+    case "F":
+    //retrieve coordinate pos and increment
+  }
 }
 
-//main function to launch robot here
+function spliceIntoChunks(arr, chunkSize) {
+  const res = [];
+  let sanitizedArray = arr.filter((el) => el);
+  while (sanitizedArray.length > 0) {
+    const chunk = sanitizedArray.splice(0, chunkSize);
+    res.push(chunk);
+  }
+  return res;
+}
+
+function extractCoordinates(str) {
+  str = str.split(" ");
+  return {
+    x: Number(str[0]),
+    y: Number(str[1]),
+    pos: str[2],
+  };
+}
 
 function launchRobot(input) {
-  return input;
-  //takes input string
-  //format into array - taking array[0] as grid
-  //iterate over directions
-  //function to move robot
-  //function to checkGrid
-  //function to check if robot lost
+  let sanitizedInput = input.split("\n");
+  gridMaxCoordinates = sanitizedInput.slice(0, 1);
+  gridMaxCoordinates = gridMaxCoordinates[0].split(" ").map(Number);
+  let remainingInput = sanitizedInput.slice(1, input.length - 1);
+  let directions = spliceIntoChunks(remainingInput, 2);
+  for (let direction of directions) {
+    coordinates = extractCoordinates(direction[0]);
+    commands = direction[1].split("");
+    commands.forEach((command) => moveRobot(command));
+  }
 }
 
 function checkRobotGridBoundaries(input) {
@@ -39,4 +67,7 @@ LLFFFLFLFL`
 
 module.exports = {
   launchRobot,
+  moveRobot,
+  extractCoordinates,
+  spliceIntoChunks,
 };
--- index.test.js
+++ index.test.js
@@ -1,21 +1,59 @@
-const { launchRobot } = require("./index");
+const {
+  launchRobot,
+  extractCoordinates,
+  spliceIntoChunks,
+  moveRobot,
+} = require("./index");
 
-test("Should intake a string with instructions, and output a string of coordinates and status", () => {
-  const inputString = `5 3
-1 1 E
-LFLFLFLF
+// test("Should intake string commands and update coordinates each time it is run", () => {
+//   const compass = ["N", "E", "S", "W"];
+//   let coordinates = { x: "1", y: "1", pos: "E" };
+//   const command = "L";
+//   let gridMaxCoordinates = [5, 3];
+//   expect(moveRobot(command)).toEqual({ x: 2, y: 3, pos: "E" });
+// });
 
-3 2 N
-FRRFLLFFRRFLL
-
-0 3 W
-LLFFFLFLFL`;
-
-  const launchRobotWithString = launchRobot(inputString);
-  const expectedOutput = `1 1 E
-3 3 N LOST
-2 3 S
-`;
-
-  expect(launchRobotWithString).toBe(expectedOutput);
+test("Should intake string input of coordinates format it into an object", () => {
+  const formatCoordinatesIntoObject = extractCoordinates("1 1 E");
+  expect(formatCoordinatesIntoObject).toEqual({ x: 1, y: 1, pos: "E" });
 });
+
+test("Should intake array of strings and format it into an array of arrays without any whitespaces", () => {
+  let inputArray = [
+    "1 1 E",
+    "LFLFLFLF",
+    "",
+    "3 2 N",
+    "FRRFLLFFRRFLL",
+    "",
+    "0 3 W",
+    "LLFFFLFLFL",
+  ];
+  const arrayToSplice = spliceIntoChunks(inputArray, 2);
+  const expectedOutput = [
+    ["1 1 E", "LFLFLFLF"],
+    ["3 2 N", "FRRFLLFFRRFLL"],
+    ["0 3 W", "LLFFFLFLFL"],
+  ];
+  expect(arrayToSplice).toEqual(expectedOutput);
+});
+
+// test("Should intake a string with instructions, and output a string of coordinates and status", () => {
+//   const inputString = `5 3
+// 1 1 E
+// LFLFLFLF
+
+// 3 2 N
+// FRRFLLFFRRFLL
+
+// 0 3 W
+// LLFFFLFLFL`;
+
+//   const launchRobotWithString = launchRobot(inputString);
+//   const expectedOutput = `1 1 E
+// 3 3 N LOST
+// 2 3 S
+// `;
+
+//   expect(launchRobotWithString).toBe(expectedOutput);
+// });
